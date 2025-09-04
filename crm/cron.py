8581390f39cf7670a9f8cb6c16436dc6732ce7aa
Aymane-Guelbaoui <aymaneguelbaoui@gmail.com>
2025-09-04 18:25:26 +0100

    Create cron.py

---
+++ crm/cron.py
@@ -0,0 +1,6 @@
+import datetime
+
+def log_crm_heartbeat():
+    timestamp = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+    with open("/tmp/crm_heartbeat_log.txt", "a") as f:
+        f.write(f"{timestamp} CRM is alive\n")
