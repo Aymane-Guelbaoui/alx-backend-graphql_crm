fc2c38875063ba143fe09e7c8eddd2431f7a263b
Aymane-Guelbaoui <aymaneguelbaoui@gmail.com>
2025-09-04 18:28:21 +0100

    Update cron.py

--- crm/cron.py
+++ crm/cron.py
@@ -4,3 +4,4 @@ def log_crm_heartbeat():
     timestamp = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
     with open("/tmp/crm_heartbeat_log.txt", "a") as f:
         f.write(f"{timestamp} CRM is alive\n")
+from gql.transport.requests import RequestsHTTPTransport", "from gql import", "gql", "Client"
